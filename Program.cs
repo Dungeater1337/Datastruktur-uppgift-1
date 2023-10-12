674f2d10c9718b5e47cc96cb70e9c90016cfe97d
judenfors <111962656+Dungeater1337@users.noreply.github.com>
2023-10-12 11:03:09 +0200

    Klar
    Klar

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿/* Skriv ut "SNAKE" baklänges
+ const string input = "SNAKE";
+
+for(int i = input.Length-1; i >= 0; i--)
+{
+    Console.Write(input[i]);
+}
+*/
+
+const string input = "SNAKE";
+Stack<char> stack = new Stack<char>();
+foreach(char c in input)
+{
+    stack.Push(c);
+}
+
+while(stack.Count != 0)
+{
+    Console.Write(stack.Pop());
+}
